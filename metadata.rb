4f201cf3866e187e3e4a4dbf3cf5290e416b5c90
Ramesh Kumar <rmekum@amazon.com>
2018-10-03 17:29:45 +1000

    Commenting

--- metadata.rb
+++ metadata.rb
@@ -10,11 +10,11 @@ version '1.11.0'
 chef_version '~> 12.0' if respond_to?(:chef_version)
 
 #depends 'apt', '< 7.0'
-depends 'nginx'
-depends 'logrotate'
-depends 'ruby-ng'
-depends 's3_file'
-depends 'sudo'
+#depends 'nginx'
+#depends 'logrotate'
+#depends 'ruby-ng'
+#depends 's3_file'
+#depends 'sudo'
 
 supports 'amazon', '>= 2017.03'
 supports 'ubuntu', '>= 16.04'
